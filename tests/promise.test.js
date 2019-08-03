854a5f8d006596fd72d137dc5c2d481d5ae73213
svipas <benas.svipas@gmail.com>
2019-08-03 23:40:40 +0300

    Fix ESLint error in tests

--- tests/promise.test.js
+++ tests/promise.test.js
@@ -1,26 +1,22 @@
 const cachimo = require('../src/cachimo');
 
 test('promise', async () => {
-  try {
-    const promise = cachimo.put('key', 'value', 1000);
-    expect(promise.constructor).toBe(Promise);
+  const promise = cachimo.put('key', 'value', 1000);
+  expect(promise.constructor).toBe(Promise);
 
-    expect(cachimo.put('key', 'value')).toBeFalsy();
-    expect(cachimo.has('key')).toBeTruthy();
-    expect(cachimo.get('key')).toBe('value');
-    expect(cachimo.size()).toBe(1);
+  expect(cachimo.put('key', 'value')).toBeFalsy();
+  expect(cachimo.has('key')).toBeTruthy();
+  expect(cachimo.get('key')).toBe('value');
+  expect(cachimo.size()).toBe(1);
 
-    const { key, value, timeout } = await promise;
-    expect(key).toBe('key');
-    expect(value).toBe('value');
-    expect(timeout).toBe(1000);
+  const { key, value, timeout } = await promise;
+  expect(key).toBe('key');
+  expect(value).toBe('value');
+  expect(timeout).toBe(1000);
 
-    expect(cachimo.has('key')).toBeFalsy();
-    expect(cachimo.get('key')).toBe(undefined);
-    expect(cachimo.size()).toBe(0);
-  } catch (err) {
-    throw err;
-  }
+  expect(cachimo.has('key')).toBeFalsy();
+  expect(cachimo.get('key')).toBe(undefined);
+  expect(cachimo.size()).toBe(0);
 });
 
 test('element remove before timeout', () => {
